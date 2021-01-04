efd304c4c76f7e2fc2692f082bf1870808db9922
Andreea Fonoage <andreea_cj83@yahoo.com>
2021-01-04 22:17:54 +0200

    Add project files.

---
+++ Data/Fonoage_Andreea_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Fonoage_Andreea_Lab8.Models;
+
+namespace Fonoage_Andreea_Lab8.Data
+{
+    public class Fonoage_Andreea_Lab8Context : DbContext
+    {
+        public Fonoage_Andreea_Lab8Context (DbContextOptions<Fonoage_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fonoage_Andreea_Lab8.Models.Book> Book { get; set; }
+    }
+}
